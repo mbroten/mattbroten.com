fcaa5f7174be31720aa01f23abfeb61fddf0192e
Matt Broten <mattbroten@gmail.com>
2012-03-09 01:15:16 -0800

    Added nav menu and backbone.js router to retrieve content via asynchronous request.

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,37 @@
 /* Author: Matt Broten
 
 */
-
 $(function(){
+  var $loadingIndicator = $('img#loading-indicator');
+  var $target = $('article#main-target');
+  var getContent = function(url){
+    $target.fadeOut('slow', function(){
+      $loadingIndicator.show();
+      $.get(url, function(data) {
+        $target.html(data);
+        $loadingIndicator.fadeOut('slow', function(){
+          $target.fadeIn('slow');
+        });
+      });
+    });
+  };
+  
+  var VALID_ACTIONS = ['/resume', '/papers'];
+  var AppRouter = Backbone.Router.extend({
+    routes: {
+      "*actions": "defaultRoute"
+    },
+    defaultRoute: function(action){
+      if (_.include(VALID_ACTIONS, action)){
+        getContent(action + '.html');
+        return;
+      }
+      getContent('/home.html');
+    }
+  });
+  var app_router = new AppRouter;
+  Backbone.history.start();
+
   var $logoImage = $('img#logo-image');
   $logoImage.load(function(){
      $logoImage.fadeIn(1000);
