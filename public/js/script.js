fa9709d4450c21ef56e69286a3b74aefd9154f30
Matt Broten <mattbroten@gmail.com>
2012-03-08 19:54:36 -0800

    Added background gradient. Positioned footer at bottom of page. Added JavaScript to fade in header image after it is loaded.

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,13 @@
-/* Author:
+/* Author: Matt Broten
 
 */
 
-
-
-
-
+$(function(){
+  var $logoImage = $('img#logo-image');
+  $logoImage.load(function(){
+     $logoImage.fadeIn(1000);
+  });
+  if ($logoImage.prop('complete')){
+    $logoImage.fadeIn(1000);
+  }
+});
